f822a4258831f268495fdc910bf0e88c76dff2a3
VCheckS <103640637+VCheckS@users.noreply.github.com>
2023-04-18 19:03:42 -0700

    Auto Test

---
+++ src/test/java/TestDelivery.java
@@ -0,0 +1,41 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class TestDelivery {
+    @Test
+    public String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+
+    }
+
+    @Test
+    public void shouldTestV1() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.timeout = 15;
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Казань");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL + "A", Keys.DELETE));
+        String currentDate = generateDate(3, "dd.MM.yyyy");
+        $("[data-test-id=date] input").setValue(currentDate);
+        $("[data-test-id=name] input").setValue("Иванов Иван Сергеевич");
+        $("[data-test-id=phone] input").setValue("+79998887766");
+        $("[data-test-id=agreement]").click();
+        SelenideElement button = $x("//*[contains(text(), 'Забронировать')]");
+        button.click();
+        $x("//*[contains(text(),'Встреча успешно забронирована на ' )]").shouldBe(Condition.visible, Duration.ofSeconds(15));
+
+    }
+
+}
+
+
